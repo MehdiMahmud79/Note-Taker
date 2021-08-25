6365c2dcab0f69fbda00774729c4d8590904808c
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-08-25 14:12:33 +0100

    commenting

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const fs = require('fs');
+const fs = require("fs");
 const router = express.Router();
-const dbNotes = require('../db/db.json');
-const {logger,jsonMiddleWare , urlMiddleWarre,uuid}= require('../helpers/middleWares');
+const dbNotes = require("../db/db.json");
+const {
+  logger,
+  jsonMiddleWare,
+  urlMiddleWarre,
+  uuid,
+} = require("../helpers/middleWares");
 
 const writeToFile = (dbPath, content) =>
- fs.writeFile(dbPath, JSON.stringify(content, null, 4), (err) =>
-   err ? console.error(err) : console.info(`\nData written to ${dbPath} successfully!`)
-);
+  fs.writeFile(dbPath, JSON.stringify(content, null, 4), (err) =>
+    err
+      ? console.error(err)
+      : console.info(`\nData written to ${dbPath} successfully!`)
+  );
 
 const updateDb = (file, content) => {
-  fs.readFile(file, 'utf8', (err, previousNotes) => {
+  fs.readFile(file, "utf8", (err, previousNotes) => {
     if (err) {
       console.error(err);
     } else {
@@ -19,69 +26,59 @@ const updateDb = (file, content) => {
       writeToFile(file, parsedNotes);
     }
   });
- };
+};
 // get request to the all notes
-router.get('/', (req, res) => res.json(dbNotes));
+router.get("/", (req, res) => res.json(dbNotes));
 
 // post a new note to the server
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   console.info(`Adding a new note due to receiving: ${req.method} request.`);
   const { title, text } = req.body;
-  if (req.body) {
-    const receivedNote = {
-      title,
-      text,
-      id: uuid(),
-    };
+  if (!req.body) res.error(`Error in posting ${receivedNote.title}.`);
+  const receivedNote = {
+    title,
+    text,
+    id: uuid(),
+  };
 
-    updateDb('./db/db.json', receivedNote);
-    res.json(`Note ${receivedNote.title} added successfully.`);
-  } else {
-    res.error(`Error in posting ${receivedNote.title}.`);
-  }
+  updateDb("./db/db.json", receivedNote);
+  res.json(`Note ${receivedNote.title} added successfully.`);
 });
 
+// delete a note from the list
 router.delete(`/:id`, (req, res) => {
-  console.log(`${req.method} request received`)
+  console.log(`${req.method} request received`);
   var notes = dbNotes;
   var reqIdToDelete = req.params.id;
 
-  const requiredNote=notes.filter(noteEl=> noteEl.id==reqIdToDelete);
-  const requiredIndex = notes.findIndex(el => el.id == reqIdToDelete);
+  const requiredNote = notes.filter((noteEl) => noteEl.id == reqIdToDelete);
+  const requiredIndex = notes.findIndex((el) => el.id == reqIdToDelete);
 
-  if(!requiredNote){
+  if (!requiredNote) {
     const response = {
-      status: 'failure',
+      status: "failure",
       body: `note with ${reqIdToDelete} not found`,
     };
-      console.log(response)
-      res.json(response);
+    console.log(response);
+    res.json(response);
   }
   // requiredNote=notes[noteIndexToDelete];
-  notes.splice(requiredIndex,1);
+  notes.splice(requiredIndex, 1);
   // stringify notes
-const updatedNotes=JSON.stringify(notes);
-        fs.writeFile(`./db/db.json`, updatedNotes, `utf8`, (err) =>
-       err
-         ? console.error(err)
-         : console.log(
-             `Note ${requiredNote} has been deleted from the database!`
-           )
-     );  
- 
-     const response = {
-      status: 'success',
-      body: { note: requiredNote,
-              action: "deleted"
-            } ,
-       };
-        console.log(response)
-        res.json(response);
-       return;               
-      
+  const updatedNotes = JSON.stringify(notes);
+  fs.writeFile(`./db/db.json`, updatedNotes, `utf8`, (err) =>
+    err
+      ? console.error(err)
+      : console.log(`Note ${requiredNote} has been deleted from the database!`)
+  );
+
+  const response = {
+    status: "success",
+    body: { note: reqIdToDelete, action: "deleted" },
+  };
+  console.log(response);
+  res.json(response);
+  return;
 });
-
- 
-
 
 module.exports = router;
